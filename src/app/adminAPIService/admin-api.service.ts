18e8a1e9ef13b0d2036395cd7d0f61419be58642
Vaisakh Vijayan <evvaisakh4@gmail.com>
2024-05-12 20:19:12 +0530

    first commit

---
+++ src/app/adminAPIService/admin-api.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminAPIService {
+
+  server_url: string = "http://localhost:3000"
+  constructor(private http: HttpClient, private router: Router) { }
+
+  authenticateAPI(email: string, password: string) {
+    return this.http.get(`${this.server_url}/users/1`).subscribe((result: any) => {
+      if (result.email == email && result.password == password) {
+        sessionStorage.setItem("adminDetails", JSON.stringify(result))
+        alert("Login success")
+        this.router.navigateByUrl('dashboard')
+      } else {
+        alert("Invalid email / password")
+      }
+    })
+  }
+}
---
+++ src/app/modules/users/services/api.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { userModel } from '../users.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  server_url = "http://localhost:3000"
+  constructor(private http: HttpClient) { }
+
+  saveUserAPI(user: userModel) {
+    return this.http.post(`${this.server_url}/users`, user)
+  }
+
+  getAllUsersAPI() {
+    return this.http.get(`${this.server_url}/users`)
+  }
+  
+}
